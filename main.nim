bf19bfc607d3c70bc8221e14ca5b2aaffc79856e
Christopher Dunn <cdunn2001@gmail.com>
2017-03-09 14:49:05 -0600

    main runner

---
+++ main.nim
@@ -0,0 +1,12 @@
+# vim: sw=2 ts=2 sts=2 tw=80 et:
+from common import nil
+from falcon import nil
+from DW_banded import nil
+from kmer_lookup import nil
+from poo import nil
+
+proc main() =
+  echo "main"
+when isMainModule:
+  #threadpool.setMaxPoolSize(1) #n_cores)
+  main()
